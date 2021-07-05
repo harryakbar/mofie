5fc4705ba4b72d53d21a25f2fd67247156e24cfa
harryakbar <harryakbaram@gmail.com>
2021-07-05 21:37:54 +0700

    Initial Commit

---
+++ Mofie/MofieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MofieApp.swift
+//  Mofie
+//
+//  Created by Harry Akbar Ali Munir on 05/07/21.
+//
+
+import SwiftUI
+
+@main
+struct MofieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
